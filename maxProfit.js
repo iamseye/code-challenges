598509d4a97f8fee49c2f5f885b9bac07e9fcf43
jessiehuang <iamseye@gmail.com>
2022-05-07 13:58:31 -0400

    max Profit

--- maxProfit.js
+++ maxProfit.js
@@ -10,17 +10,31 @@ var maxProfit = function(prices) {
     }
 
     let profit = 0;
+    let min = prices[0] ;
 
     for (let i = 0; i < prices.length; ++i) {
-        if (prices[i] > prices[i-1]) {
-            profit += prices[i] - prices[i-1];
+        const next = prices[i+1];
+        const current = prices[i];
+        if (current< next) {
+            if(current < min) {
+                min = current;
+            }
+
+            const thisProfit = next-current > next-min ? next-current : next-min
+            if (thisProfit > profit) {
+                profit = thisProfit
+            }
         }
     }
+
 
     return profit;
 };
 
- console.log(maxProfit([7,1,5,3,6,4]))
-console.log(maxProfit([1,2,3,4,5]))
-console.log(maxProfit([7,6,4,3,1]))
-console.log(maxProfit([6,1,3,2,4,7]));
+console.log(maxProfit([7,1,5,3,6,4])) //5
+// console.log(maxProfit([1,2,3,4,5]))
+// console.log(maxProfit([7,6,4,3,1]))
+// console.log(maxProfit([6,1,3,2,4,7]));
+//console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([2,1,2,0,1])); //1
+console.log(maxProfit([3,2,6,5,0,3])) //4
